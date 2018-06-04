d45d5a25f54aa11dfee7c0409ebd963dd7945077
Luca Braun <l.braun@rto.de>
2018-06-04 17:01:28 +0200

    Completed create.bat, except the json and version part

--- create.bat
+++ create.bat
@@ -41,12 +41,23 @@ exit /B 0
 
 :: Creates the configuration file for sami.phar.
 :createConfig
+set versions=
+set github=false
+set ghUser=
+set ghRepo=
+
 :: Ask user for necessary information about the configuration file.
 set /P title="Title of the documentation: "
 set /P projectDir="Full path to the project directory (e.g. C:/projects/code): "
 set /P docDir="Directory which should documented (e.g. Lib): "
 set /P outDir="Output directory name (e.g. Doc): "
 set /P excludeDirs="Exclude directories (e.g. Test Resources): "
+
+call :choice "Use github?" yes no github
+if "%github%" == "true" (
+  call :setGhUser
+  call :setGhRepo
+)
 
 set cacheDir=%projectDir%/samiCache
 
@@ -55,6 +66,24 @@ echo ^<?php> %config%
 echo.>>%config%
 echo use Sami\Sami;>> %config%
 echo use Symfony\Component\Finder\Finder;>> %config%
+
+if "%github%" == "true" (
+  echo use Sami\RemoteRepository\GitHubRemoteRepository;>> %config%
+  echo use Sami\Version\GitVersionCollection;>> %config%
+)
+
+echo.>>%config%
+echo $dir = '%projectDir%';>> %config%
+
+if "%github%" == "true" (
+  echo.>>%config%
+  echo $versions = GitVersionCollection::create( $dir )>> %config%
+
+  :: For loop with version numbers and json decoding
+
+  echo     -^>add( 'master', 'master' );>> %config%
+)
+
 echo.>>%config%
 echo $iterator = Finder::create()>> %config%
 echo     -^>files()>> %config%
@@ -64,15 +93,55 @@ for %%e in (%excludeDirs%) do (
   echo     -^>exclude( '%%e' ^)>> %config%
 )
 
-echo     -^>in( '%projectDir%/%docDir%' );>> %config%
+echo     -^>in( $dir . '/%docDir%' );>> %config%
+
 echo.>>%config%
 echo $options = [>> %config%
 echo     'title' =^> '%title%',>> %config%
-echo     'build_dir' =^> '%projectDir%/%outDir%',>> %config%
-echo     'cache_dir' =^> '%projectDir%/samiCache',>> %config%
+
+if "%github%" == "true" (
+  echo     'build_dir' =^> $dir . '/%outDir%/\%version%',>> %config%
+  echo     'cache_dir' =^> $dir . '/samiCache/\%version%',>> %config%
+) else (
+  echo     'build_dir' =^> $dir . /%outDir%',>> %config%
+  echo     'cache_dir' =^> $dir . /samiCache',>> %config%
+)
+
 echo     'default_opened_level' =^> 2,>> %config%
+
+if "%github%" == "true" (
+  echo     'versions' =^> $versions,>> %config%
+  echo     'remote_repository' =^> new GitHubRemoteRepository( '%ghUser%/%ghRepo%', $dir ),>> %config%
+)
+
 echo ];>> %config%
 echo.>>%config%
 echo return new Sami( $iterator, $options );>> %config%
 :: File finished.
+exit /B 0
+
+:choice
+set /P answer=%1
+if /I "%answer%" equ "Y" goto :%2 %4
+if /I "%answer%" equ "N" goto :%3 %4
+exit /B 0
+
+:yes
+set %4=true
+exit /B 0
+
+:no
+set %4=false
+exit /B 0
+
+:setGhUser
+set /P ghUser="Github username: "
+
+if [%ghUser%] == [] goto :setGhUser
+exit /B 0
+
+:setGhRepo
+set /P ghRepo="Github repository name: "
+
+if [%ghRepo%] == [] goto :setGhRepo
 exit /B 0
